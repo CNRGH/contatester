6db12aea5e4a783825bab77069acfa8d917a10e5
delafoy <damien.delafoy@cnrgh.fr>
2018-10-31 17:27:55 +0100

    add args -A -d ; add_machine_param() for cluster parameter; add nb_tasks() ; modifie function write_batch_file

--- contatester/src/fr/cea/lbi/contatester/__main__.py
+++ contatester/src/fr/cea/lbi/contatester/__main__.py
@@ -3,12 +3,13 @@
 from pathlib import Path
 from os import access, R_OK, getcwd, makedirs, remove, popen
 from os.path import isfile, abspath, isdir, join, exists
-from typing import Sequence, Tuple, List, BinaryIO
+from typing import Sequence, Tuple, List, BinaryIO, Dict, Union
 import argparse
 import io
 import subprocess
 import sys
 import glob
+from datetime import datetime
 
 script_name = "contaTester"
 
@@ -37,7 +38,7 @@ def valid_output_dir(prospective_path: str) -> str:
 
 
 def get_cli_args(parameters: Sequence[str] = sys.argv[1:]) \
-        -> Tuple[List[str], str, str, bool, str]:
+        -> Tuple[List[str], str, str, bool, str, str, str]:
     """Parse command line parameters
     Parse program parameters using argparse module
     Args:
@@ -82,6 +83,13 @@ def get_cli_args(parameters: Sequence[str] = sys.argv[1:]) \
     parser.add_argument("-m", "--mail", default="", type=str,
                         help="send an email at the end of the job")
 
+    parser.add_argument("-A", "--accounting", default="", type=str,
+                        help="msub option for calculation time imputation")
+
+    parser.add_argument("-d", "--dagname", default=default_dagfile_name(),
+                        type=str,
+                        help="DAG file name for pegasus")
+
     # keep arguments
     args = parser.parse_args(parameters)
 
@@ -90,6 +98,8 @@ def get_cli_args(parameters: Sequence[str] = sys.argv[1:]) \
     out_dir = abspath(args.outdir)
     check = args.check
     mail = args.mail
+    accounting = args.accounting
+    dagname = args.dagname
 
     if vcf_list is not None:
         with open(vcf_list, 'r') as filin:
@@ -105,7 +115,14 @@ def get_cli_args(parameters: Sequence[str] = sys.argv[1:]) \
     else:
         report = ""
 
-    return vcfs, out_dir, report, check, mail
+    return vcfs, out_dir, report, check, mail, accounting, dagname
+
+
+def default_dagfile_name() -> str :
+    dt = datetime.today()
+    dag_filename = ("contatest_" + str(dt.year) + str(dt.month) + str(dt.day) +
+                    str(dt.hour) + str(dt.minute) + str(dt.second) + ".dagfile")
+    return dag_filename
 
 
 def write_binary(file_handler: BinaryIO, statement: str) -> None:
@@ -237,7 +254,7 @@ def write_dag_file(check: bool, dag_file: str, out_dir: str, report: str,
 
 
 def write_batch_file(dag_file: str, mail: str, msub_file: str, nb_task: int,
-                     out_dir: str) -> None:
+                     out_dir: str, accounting: str) -> None:
     """Write a Batch file to be processed by SLURM
 
     Args:
@@ -246,52 +263,138 @@ def write_batch_file(dag_file: str, mail: str, msub_file: str, nb_task: int,
         msub_file: path to write the batch file
         nb_task: number of tasks to process
         out_dir: Directory to put results
+        accounting: msub option for calculation time imputation
     """
     with open(msub_file, "wb", ) as msub_f:
-        # Parametres MSUB
-        write_binary(msub_f,
-                     "#!/bin/bash\n" +
-                     "#MSUB -r " + script_name + "\n" +
-                     "#MSUB -n " + str(nb_task) + "\n" +
-                     "#MSUB -c 14 \n" +
-                     "#MSUB -T 86400 \n" +
-                     "#MSUB -o " + out_dir + script_name + "%j.out\n" +
-                     "#MSUB -e " + out_dir + script_name + "%j.err\n")
+        clust_param = machine_param()
 
-        if len(mail) > 0:
-            write_binary(msub_f, "#MSUB -@ " + mail + ":end\n")
+        if clust_param.get("cea_clust") :
+            # Parametres MSUB
+            write_binary(msub_f,
+                         "#!/bin/bash\n" +
+                         "#MSUB -r " + script_name + "\n" +
+                         "#MSUB -n " + str(nb_task) + "\n" +
+                         "#MSUB -o " + out_dir + script_name + "%j.out\n" +
+                         "#MSUB -e " + out_dir + script_name + "%j.err\n")
+            # Clusters parameters
+            write_binary(msub_f, clust_param.get("msub_info"))
 
-        # Clusters parameters
-        hostname = popen("dnsdomainname").read()
-        if "cng.fr" in hostname or "cnrgh.fr" in hostname:
-            # lirac
-            msub_f.write(b"#MSUB -q normal\n")
-        elif "ccrt.ccc.cea.fr" in hostname:
-            # cobalt
-            msub_f.write(b"#MSUB -A fg0062\n#MSUB -q broadwell\n")
+            if len(mail) > 0:
+                write_binary(msub_f, "#MSUB -@ " + mail + ":end\n")
 
-        # MODULES LOAD
-        write_binary(msub_f,
-                     "module load extenv/ig\n" +
-                     "module load pegasus\n" +
-                     "module load bcftools\n" +
-                     "module load bedtools\n" +
-                     "module load bedops\n" +
-                     "module load useq\n" +
-                     "mpirun -oversubscribe -n " + str(nb_task + 1) +
-                     " pegasus-mpi-cluster " + dag_file + "\n")
+            if len(accounting) > 0:
+                write_binary(msub_f, "#MSUB -A " + accounting + "\n")
+
+            # Clusters parameters
+
+            # hostname = popen("dnsdomainname").read()
+            # if "cng.fr" in hostname or "cnrgh.fr" in hostname:
+            #     # lirac
+            #     write_binary(msub_f, "#MSUB -q normal\n")
+            # elif "ccrt.ccc.cea.fr" in hostname:
+            #     # cobalt
+            #     write_binary(msub_f, "#MSUB -A fg0062\n#MSUB -q broadwell\n")
+
+            # MODULES LOAD
+            write_binary(msub_f, clust_param.get("msub_module_load"))
+            # write_binary(msub_f,
+            #              "module load extenv/ig\n" +
+            #              "module load pegasus\n" +
+            #              "module load bcftools\n" +
+            #              "module load bedtools\n" +
+            #              "module load bedops\n" +
+            #              "module load useq\n")
+
+            # PEGASUS Command
+
+            mpi_exe = clust_param.get("mpi_exe")
+            mpi_opt = clust_param.get("mpi_opt")
+
+            # host_cpus = param[4]
+            write_binary(msub_f, mpi_exe + " " + mpi_opt + " -n " +
+                         str(nb_task + 1) + " pegasus-mpi-cluster " +
+                         dag_file + "\n")
+            # write_binary(msub_f, mpi_exe + " " + mpi_opt + " -n " +
+            #              str(nb_task + 1) + " pegasus-mpi-cluster --host-cpus " +
+            #              str(host_cpus) + " " + dag_file + "\n")
+        else :
+            write_binary(msub_f, "#!/bin/bash\n" + "pegasus-mpi-cluster " +
+                         dag_file + "\n")
 
 
-# Main
-def main():
-    vcfs, out_dir, report, check, mail = get_cli_args()
+def machine_param() -> Dict[str, Union[bool, str]]:
+    """ Test machine and apply a configuration
+    Usage :
+    clust_param = machine_param()
+    mpi_exe = clust_param.get("mpi_exe")
+    mpi_opt = clust_param.get("mpi_opt")
+    tasks = 10
+    dag = "dag.txt"
+    cmd = mpi_exe + " " + mpi_opt + " -n " + str(tasks) +
+    " pegasus-mpi-cluster " + dag
 
-    dag_file = join(out_dir, script_name + ".dagfile")
-    msub_file = join(out_dir, script_name + ".msub")
-    if isfile(dag_file):
-        remove(dag_file)
-    task_fmt = "TASK {id} -c {core} bash -c "
-    write_dag_file(check, dag_file, out_dir, report, task_fmt, vcfs)
+    :return: dictionary
+    """
+
+    common_load = ("module load pegasus\n" +
+                   "module load bcftools\n" +
+                   "module load bedtools\n" +
+                   "module load bedops\n" +
+                   "module load useq\n")
+
+    if isdir("/ccc"):
+        # si machine cobalt
+        cea_clust = True
+        batch_exe = "ccc_msub"
+        run_exe = "ccc_mprun"
+        mpi_exe = "ccc_mprun"
+        mpi_opt = "-E '--overcommit '"
+        nb_core = 14
+        # host_cpus = 28
+        msub_info = ("#MSUB -c " + str(nb_core) + " \n" +
+                     "#MSUB -q broadwell\n" +
+                     "#MSUB -T 86400 \n")
+        msub_module_load = ("module load extenv/ig\n" +
+                            common_load)
+    elif isdir("/env/cnrgh"):
+        # si machine cnrgh
+        cea_clust = True
+        batch_exe = "sbatch"
+        run_exe = "srun"
+        mpi_exe = "mpirun"
+        mpi_opt = "-E '--oversubscribe'"
+        nb_core = 7
+        # host_cpus = 32
+        msub_info = ("#MSUB -c " + str(nb_core) + " \n" +
+                     "#MSUB -q normal\n" +
+                     "#MSUB -T 86400 \n")
+        msub_module_load = common_load
+    else:
+        # Default machine
+        cea_clust = False
+        batch_exe = ""
+        run_exe = ""
+        mpi_exe = ""
+        mpi_opt = ""
+        nb_core = 4
+        # host_cpus = ""
+        msub_info = ""
+        msub_module_load = ""
+
+    clust_param = {}
+    clust_param["cea_clust"] = cea_clust
+    clust_param["batch_exe"] = batch_exe
+    clust_param["run_exe"] = run_exe
+    clust_param["mpi_exe"] = mpi_exe
+    clust_param["mpi_opt"] = mpi_opt
+    clust_param["nb_core"] = nb_core
+    # clust_param["host_cpus"] = host_cpus
+    clust_param["msub_module_load"] = msub_module_load
+    clust_param["msub_info"] = msub_info
+
+    return clust_param
+
+def nb_tasks(vcfs:List[str]) -> int:
 
     nb_vcf = len(vcfs)
     if nb_vcf > 48:
@@ -300,19 +403,37 @@ def main():
         nb_task = nb_vcf
     else:
         nb_task = 1
+    return nb_task
 
-    write_batch_file(dag_file, mail, msub_file, nb_task, out_dir)
+# Main
+def main():
+    vcfs, out_dir, report, check, mail, accounting, dagname = get_cli_args()
 
-    # Start Script
+    dag_file = join(out_dir, dagname)
+    msub_file = join(out_dir, dagname[0:-8] + ".msub")
+    if isfile(dag_file):
+        remove(dag_file)
+    task_fmt = "TASK {id} -c {core} bash -c "
+    write_dag_file(check, dag_file, out_dir, report, task_fmt, vcfs)
+
+    nb_task = nb_tasks(vcfs)
+    write_batch_file(dag_file, mail, msub_file, nb_task, out_dir, accounting)
+
+    # remove rescue file and ressource file
     res_files = glob.glob(dag_file + ".res*")
     for res in res_files:
         if isfile(res):
             remove(res)
 
-    cmd = ["/usr/bin/ccc_msub", msub_file]
+    # Start Script
+
+    clust_param = machine_param()
+    batch_exe = clust_param.get("batch_exe")
+    cmd = [batch_exe, msub_file]
+
     p = subprocess.call(cmd)
     if p != 0:
-        print("Error while running ccc_msub: {} ".format(" ".join(cmd)),
+        print("Error while running contatester: {} ".format(" ".join(cmd)),
               file=sys.stderr)
     sys.exit(p)
 
@@ -320,3 +441,4 @@ def main():
 if __name__ == '__main__':
     # execute only if run as a script
     main()
+
