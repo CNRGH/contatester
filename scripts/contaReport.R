e535cb5709339ade58d3a7ead64329dd1349674b
Jonathan MERCIER <jonathan.mercier@cnrgh.fr>
2019-02-12 18:12:14 +0100

    check if R required libraries are installed if not install them

--- scripts/contaReport.R
+++ scripts/contaReport.R
@@ -20,11 +20,17 @@
 ### Libraries
 ###############
 
+check.packages <- function (pkg) {
+  print("Installing required packages, please wait...")
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) {
+    install.packages(new.pkg, dependencies = TRUE)
+  }
+  sapply(pkg, library, character.only = TRUE)
+}
 
-library("optparse")
-library("grid")
-library("gridBase")
-library("gridExtra")
+packages <- c("optparse", "grid", "gridBase", "gridExtra")
+check.packages(packages)
 
 #############
 ### function
