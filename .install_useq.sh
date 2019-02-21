7f8732b295dfa9dedbebbaef475a03b8843b79b9
Jonathan MERCIER <jonathan.mercier@cnrgh.fr>
2019-02-21 17:11:28 +0100

    Execute jar file

--- .install_useq.sh
+++ .install_useq.sh
@@ -2,7 +2,11 @@
 declare -r prefix=$(readlink -f "$1")
 [[ ! -e USeq_9.2.0.zip ]] && curl -LO https://github.com/HuntsmanCancerInstitute/USeq/releases/download/USeq_9.2.0/USeq_9.2.0.zip
 [[ ! -e USeq_9.2.0 ]] && unzip USeq_9.2.0.zip
+mkdir -p "${prefix}/share/useq/"
 for filepath in USeq_9.2.0/Apps/[A-Z]*; do 
   filename=$(basename "${filepath}")
-  install -m 0755 "${filepath}" "${prefix}/bin/${filename}";
+  echo "#!/usr/bin/env bash
+java -jar '${prefix}/share/useq/${filename}' \"\$*\" " > "${prefix}/bin/${filename}"
+  chmod +x "${prefix}/bin/${filename}"
+  install -m 644 "${filepath}" "${prefix}/share/useq/${filename}";
 done
